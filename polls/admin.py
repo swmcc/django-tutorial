d6ba30ae0699877714deb1eb37479d91c58c1392
Stephen McCullough <me@swm.cc>
2013-11-18 12:59:13 +0000

    Half way through the second tutorial just sycing up

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
+from polls.models import Choice
 from polls.models import Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, 					{'fields': ['question_text']}),
+		['Date information',	{'fields': ['pub_date'], 'classes': ['collapse']}],
+	]
+	inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
